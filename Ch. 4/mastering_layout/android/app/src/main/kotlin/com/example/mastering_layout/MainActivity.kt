35499e8a6ada37bd44b56171b0f72ec96407df6e
thisfren <fretzzzo@gmail.com>
2025-05-13 14:27:54 +0300

    Chapter 4 initial commit.

---
+++ Ch. 4/mastering_layout/android/app/src/main/kotlin/com/example/mastering_layout/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mastering_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Ch. 4/mastering_layout/lib/main.dart
@@ -0,0 +1,11 @@
+// lib/main.dart
+import 'package:flutter/material.dart' show BuildContext, Placeholder, StatelessWidget, Widget;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
