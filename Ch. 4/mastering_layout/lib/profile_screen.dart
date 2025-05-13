a04d8f9ff2dce6164392a2b4bfd9acbc9217d32e
thisfren <fretzzzo@gmail.com>
2025-05-13 16:40:00 +0300

    Placing widgets one after another done.

--- Ch. 4/mastering_layout/lib/profile_screen.dart
+++ Ch. 4/mastering_layout/lib/profile_screen.dart
@@ -1,6 +1,12 @@
 // /lib/profile_screen.dart
 
-import 'package:flutter/material.dart' show BoxFit, BuildContext, ClipOval, Column, Container, EdgeInsets, FontWeight, Icon, IconData, Icons, Image, MainAxisAlignment, Padding, Row, Scaffold, StatelessWidget, Text, TextStyle, Widget;
+/*
+ * This is a simple profile screen that displays a profile image, name, age, and status.
+ * The Flutter tools also have a setting called Debug Paint, which you can activate from the Flutter tools in your editor or from the command line.
+ * This feature will draw lines around your widgets so that you can see in more detail how they are being rendered, which can be useful to catch layout errors.
+ */
+import 'package:flutter/material.dart' show BoxFit, BuildContext, ClipOval, Column, Container, EdgeInsets, FontWeight, Icon, IconData, Icons, Image, MainAxisAlignment, Padding, Row, Scaffold, Stack, StatelessWidget, Text, TextStyle, Transform, Widget;
+import 'package:flutter/rendering.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -8,11 +14,29 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /* 
       body: Column(
         children: <Widget>[
           _buildProfileImage(context),
           _buildProfileDetails(context),
           _buildActions(context)
+        ]
+      )
+      */
+      // In order to place widgets on top of each other, you can use a Stack widget
+      body: Stack(
+        children: <Widget>[
+          Image.asset('assets/khachik_simonian_beach_unsplash.jpg'),
+          Transform.translate(
+            offset: Offset(0, 100),
+            child: Column(
+              children: <Widget>[
+                _buildProfileImage(context),
+                _buildProfileDetails(context),
+                _buildActions(context)
+              ]
+            )
+          )
         ]
       )
     );
@@ -47,6 +71,15 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 
+  /*
+  The only real difference between a Row widget and a Column widget is the axis in which they lay out their children.
+  It's interesting that you can insert a Row widget into a Column widget and vice versa.
+  There are also two properties on Column and Row widgets that can modify how Flutter lays out your widgets:
+    CrossAxisAlignment
+    MainAxisAlignment
+  These are abstractions on the x and y axis. 
+  They are also referring to different axes depending on whether you are using a Row widget or a Column widget
+  */
   Widget _buildDetailsRow(String heading, String value) {
     return Row(
       children: <Widget>[
